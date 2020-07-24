2bd48478d39900d0aa2d77891d80f2106eb80b0a
codywall <cwall@csumb.edu>
2020-07-24 14:49:15 -0700

    Add post excerpt to PostLink

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -3,6 +3,7 @@ import { Link } from 'gatsby';
 const PostLink = ({ post }) => (
   <div>
     <Link to={post.frontmatter.slug}>{post.frontmatter.title}</Link>
+    <div dangerouslySetInnerHTML={{ __html: post.excerpt }} />
   </div>
 );
 export default PostLink;
