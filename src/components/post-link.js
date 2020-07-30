7700bbd52ccc96d7602c6c0d52f398663b3fcd57
codywall <cwall@csumb.edu>
2020-07-29 18:08:03 -0700

    Remove excerpts for now

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 const PostLink = ({ post }) => {
-  const rawExcerpt = post.excerptAst.children[0].children[0].children[0].value;
-  const strippedExcerpt = rawExcerpt.replace(/(<([^>]+)>)/gi, '');
+  // const rawExcerpt = post.excerptAst.children[0].children[0].children[0].value;
+  // const strippedExcerpt = rawExcerpt.replace(/(<([^>]+)>)/gi, '');
   return (
     <div>
       <Link to={post.frontmatter.slug}>{post.frontmatter.title}</Link>
-      <div>{strippedExcerpt}</div>
+      {/* <div>{strippedExcerpt}</div> */}
     </div>
   );
 };
