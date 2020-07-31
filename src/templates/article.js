32832ee5121d2b2c2894a3ae11bceeb5a30764b5
codywall <cwall@csumb.edu>
2020-07-31 10:11:14 -0700

    Formatting

--- src/templates/article.js
+++ src/templates/article.js
@@ -6,16 +6,16 @@ export default function Template({ data }) {
   const { frontmatter, html } = markdownRemark;
   return (
     <>
-    <Link to='/'>Back to home</Link>
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+      <Link to="/">Back to home</Link>
+      <div className="blog-post-container">
+        <div className="blog-post">
+          <h1>{frontmatter.title}</h1>
+          <div
+            className="blog-post-content"
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        </div>
       </div>
-    </div>
     </>
   );
 }
