b9d0cd40d1b363f68b5ed51ff2e6b342afd10a1d
codywall <cwall@csumb.edu>
2020-07-31 09:56:02 -0700

    Add simple back to home button

--- src/templates/article.js
+++ src/templates/article.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
 
 export default function Template({ data }) {
   const { markdownRemark } = data;
   const { frontmatter, html } = markdownRemark;
   return (
+    <>
+    <Link to='/'>Back to home</Link>
     <div className="blog-post-container">
       <div className="blog-post">
         <h1>{frontmatter.title}</h1>
@@ -14,6 +16,7 @@ export default function Template({ data }) {
         />
       </div>
     </div>
+    </>
   );
 }
 
