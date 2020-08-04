2ab379123c61305a38dd10ec5e54c94d4a96ad6c
codywall <cwall@csumb.edu>
2020-08-04 10:53:57 -0700

    Add non-functioning clear input button

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,26 @@ import PostLink from '../components/post-link';
 import Layout from '../components/layout';
 
 const SearchBox = styled('div')`
-  max-width: 500px;
+  max-width: 450px;
   margin: 30px auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 2px solid lightgrey;
+  border-radius: 2px;
+  padding-left: 15px;
+  flex-direction: row;
   input {
     outline: none;
-    width: 100%;
-    border: 2px solid lightgrey;
-    border-radius: 2px;
+    border: none;
+    width: 90%;
     background: #fff;
-    padding: 10px;
     font-family: 'Roboto', sans-serif;
     height: 50px;
     transition: all 0.05s ease-in-out;
+  }
+  span {
+    padding-right: 15px;
   }
 `;
 
@@ -58,6 +66,7 @@ export default function IndexPage(props) {
           placeholder={`Search ${data.allMarkdownRemark.totalCount} articles...`}
           onChange={handleInputChange}
         />
+        {query && <span title="Clear">&times;</span>}
       </SearchBox>
       {hasSearchResults && <h5>{filteredData.length} results</h5>}
       <PostsWrapper>
