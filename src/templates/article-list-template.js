ce2f6ba9dbddfcbee409c9ee36f8c9c6b2260a43
codywall <cwall@csumb.edu>
2020-08-28 14:44:37 -0700

    Style current page link

--- src/templates/article-list-template.js
+++ src/templates/article-list-template.js
@@ -1,7 +1,35 @@
 import React from 'react';
 import { graphql, Link } from 'gatsby';
+import styled from '@emotion/styled';
 import Layout from '../components/layout';
 import PostLink from '../components/post-link';
+
+const Pagination = styled('div')`
+  a {
+    color: black;
+    float: left;
+    padding: 8px 16px;
+    text-decoration: none;
+    border: 1px solid #ddd;
+  }
+  a.active {
+    background-color: #4caf50;
+    color: white;
+    border: 1px solid #4caf50;
+  }
+  a:hover:not(.active) {
+    background-color: #ddd;
+  }
+
+  a:first-child {
+    border-top-left-radius: 5px;
+    border-bottom-left-radius: 5px;
+  }
+  a:last-child {
+    border-top-right-radius: 5px;
+    border-bottom-right-radius: 5px;
+  }
+`;
 
 export default function ArticleList(props) {
   const posts = props.data.allMarkdownRemark.edges;
@@ -23,25 +51,36 @@ export default function ArticleList(props) {
             excerpt={node.excerpt}
           />
         );
-      })}{' '}
-      {!isFirst && (
-        <Link to={prevPage} rel="prev">
-          ← Previous Page
-        </Link>
-      )}
-      {Array.from({ length: numPages }, (_, i) => (
-        <Link
-          key={`pagination-number${i + 1}`}
-          to={`/articles/${i === 0 ? '' : i + 1}`}
-        >
-          {i + 1}
-        </Link>
-      ))}
-      {!isLast && (
-        <Link to={nextPage} rel="next">
-          Next Page →
-        </Link>
-      )}
+      })}
+      <Pagination>
+        {!isFirst && (
+          <Link to={prevPage} rel="prev">
+            ← Previous Page
+          </Link>
+        )}
+        {Array.from({ length: numPages }, (_, i) => (
+          <Link
+            style={
+              currentPage === i + 1
+                ? {
+                    backgroundColor: `#112e51`,
+                    color: `white`,
+                    border: `1px solid #112e51`,
+                  }
+                : {}
+            }
+            key={`pagination-number${i + 1}`}
+            to={`/articles/${i === 0 ? '' : i + 1}`}
+          >
+            {i + 1}
+          </Link>
+        ))}
+        {!isLast && (
+          <Link to={nextPage} rel="next">
+            Next Page →
+          </Link>
+        )}
+      </Pagination>
     </Layout>
   );
 }
