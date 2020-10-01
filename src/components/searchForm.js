bd1004676d428325d9dfabd67113ccf8031d4e2c
codywall <cody.wall@gmail.com>
2020-10-01 13:51:19 -0700

    Remove old search box

--- src/components/searchForm.js
+++ src/components/searchForm.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import { navigate } from 'gatsby';
+import styled from '@emotion/styled';
+
+const SearchBox = styled('form')`
+  display: flex;
+  flex-direction: column;
+  input {
+    display: flex;
+    margin: 1.5rem;
+    margin-top: 0.5rem;
+    max-width: 450px;
+    border: 2px solid lightgrey;
+    border-radius: 2px;
+    padding-left: 15px;
+    width: 90%;
+    background: #fff;
+    font-family: 'Roboto', sans-serif;
+    height: 50px;
+    transition: all 0.05s ease-in-out;
+  }
+`;
 
 const SearchForm = ({ query }) => (
-  <form role="search" method="GET">
+  <SearchBox role="search" method="GET">
     <label htmlFor="search-input">
       <h1>Search articles</h1>
     </label>
@@ -16,7 +36,6 @@ const SearchForm = ({ query }) => (
       }
       value={query}
     />
-    <button type="submit">Submit</button>
-  </form>
+  </SearchBox>
 );
 export default SearchForm;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import { graphql, StaticQuery } from 'gatsby';
-import Search from '../components/search';
-import Layout from '../components/layout';
+import React, { useState, useEffect } from 'react';
 import SEO from '../components/seo';
+import Layout from '../components/layout';
+import SearchForm from '../components/searchForm';
+import SearchResults from '../components/searchResults';
 
-export default function IndexPage() {
+const Search = () => {
+  const [results, setResults] = useState([]);
+  const searchQuery =
+    new URLSearchParams(location.search).get('keywords') || '';
+
+  useEffect(() => {
+    if (searchQuery && window.__LUNR__) {
+      window.__LUNR__.__loaded.then((lunr) => {
+        const refs = lunr.en.index.search(searchQuery);
+        const posts = refs.map(({ ref }) => lunr.en.store[ref]);
+        setResults(posts);
+      });
+    }
+  }, [location.search]);
+
   return (
     <Layout>
       <SEO title="Home" />
-      {/* <StaticQuery
-        query={graphql`
-          query SearchIndexQuery {
-            siteSearchIndex {
-              index
-            }
-          }
-        `}
-        render={(data) => <Search searchIndex={data.siteSearchIndex.index} />}
-      /> */}
+      <SearchForm query={searchQuery} />
+      <SearchResults query={searchQuery} results={results} />
     </Layout>
   );
-}
+};
+
+export default Search;
