7e64ac89d8ca1a2aee6c7b976e87bd414c6001c9
codywall <cody.wall@gmail.com>
2020-10-02 11:33:06 -0700

    Check if loaded before showing no results

--- src/components/searchResults.js
+++ src/components/searchResults.js
@@ -2,25 +2,30 @@ import React from 'react';
 import ArticleLink from './article-link';
 
 const SearchResults = ({ results, query, resultsName, isLoaded }) => {
-  console.log(isLoaded);
-  if (isLoaded && !!results.length && query) {
-    return (
-      <>
-        <h2>
-          Found {results.length} articles for "{resultsName}"
-        </h2>
-        <ul>
-          {results.map(({ slug, title, html }) => (
-            <ArticleLink key={slug} slug={slug} title={title} excerpt={html} />
-          ))}
-        </ul>
-      </>
-    );
-  }
-  if (results.length === 0 && query) {
-    return <h2>No results found for "{query}"</h2>;
-  }
-  if (!isLoaded) {
+  if (isLoaded) {
+    if (!!results.length && query) {
+      return (
+        <>
+          <h2>
+            Found {results.length} articles for "{resultsName}"
+          </h2>
+          <ul>
+            {results.map(({ slug, title, html }) => (
+              <ArticleLink
+                key={slug}
+                slug={slug}
+                title={title}
+                excerpt={html}
+              />
+            ))}
+          </ul>
+        </>
+      );
+    }
+    if (results.length === 0 && query) {
+      return <h2>No results found for "{query}"</h2>;
+    }
+  } else {
     return <h2>Loading...</h2>;
   }
   return null;
