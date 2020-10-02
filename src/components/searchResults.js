72e81bea2cfa9709ee9824ca5e7d3c05eb8bb49d
codywall <cody.wall@gmail.com>
2020-10-02 11:54:31 -0700

    Use loading spinner animation from package

--- src/components/searchResults.js
+++ src/components/searchResults.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Loader from 'react-loader-spinner';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import ArticleLink from './article-link';
 
 const SearchResults = ({ results, query, resultsName, isLoaded }) => {
@@ -26,8 +28,17 @@ const SearchResults = ({ results, query, resultsName, isLoaded }) => {
       return <h2>No results found for "{query}"</h2>;
     }
   } else {
-    return <h2>Loading...</h2>;
+    return (
+      <h3
+        style={{
+          textAlign: 'center',
+          fontWeight: '500',
+        }}
+      >
+        Initializing search engine
+        <Loader type="ThreeDots" color="#0071BC" height={60} width={60} />
+      </h3>
+    );
   }
-  return null;
 };
 export default SearchResults;
